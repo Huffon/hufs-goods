321fa7ace39baa97a45c9a96ff1038657f007a1a
huffonism@gmail.com <huffonism@gmail.com>
2018-12-13 23:21:53 +0900

    store user info

--- config/passport.js
+++ config/passport.js
@@ -22,6 +22,10 @@ passport.use('local.signup', new LocalStrategy({
     req.checkBody('email', '이메일 양식이 맞지 않습니다.').notEmpty().isEmail();
     req.checkBody('password', '비밀번호를 4자 이상으로 설정해주세요.').notEmpty().isLength({min:4});
     var errors = req.validationErrors();
+    var address = req.body.address;
+    var phone = req.body.phone;
+    var name = req.body.name;
+
     if (errors) {
         var messages = [];
         errors.forEach(function(error) {
@@ -29,16 +33,17 @@ passport.use('local.signup', new LocalStrategy({
         });
         return done(null, false, req.flash('error', messages));
     }
-    User.findOne({'email': email}, function(err, user) {
+    User.findOne({ 'email': email }, function( err, user) {
         if (err) return done(err);
         if (user) return done(null, false, {message: '이미 존재하는 이메일입니다.'});
 
         var newUser = new User();
         newUser.email = email;
+        console.log(req.body);
         newUser.password = newUser.encryptPassword(password);
-        newUser.address = 'address';
-        newUser.phone = 'phone';
-        newUser.name = 'name';
+        newUser.address = address;
+        newUser.phone = phone;
+        newUser.name = name;
         newUser.save(function(err, reuslt) {
            if(err){
             return done(err);
